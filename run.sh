158fa12c60f027285ff75c59c6d48548a84c32e8
Deven Phillips <deven.phillips@redhat.com>
2017-08-23 09:42:49 -0400

    Clean up

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ansible-galaxy install --role-path . -r labs-ci-cd-requirements.yml
+ansible-galaxy install --roles-path . -r labs-ci-cd-requirements.yml
 
 ansible-playbook --connection=local -i ci-cd-bootstrap/inventory labs-ci-cd.yml
