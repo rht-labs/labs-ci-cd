7ee29c037ad91518a9fe4e18bf6b69303efc8d12
Deven Phillips <deven.phillips@redhat.com>
2018-01-15 15:53:46 -0500

    Refactor to use RHEL 7.4 base image

--- docker/sonarqube/run.sh
+++ docker/sonarqube/run.sh
@@ -5,12 +5,17 @@ set -e
 
 ## If the mounted data volume is empty, populate it from the default data
 if ! [[ "$(ls -A /opt/sonarqube/data)" ]]; then
-    cp -a /opt/sonarqube/data-init /opt/sonarqube/data
+    cp -ar /opt/sonarqube/data-init/* /opt/sonarqube/data/
+fi
+
+## If the mounted data volume is empty, populate it from the default data
+if ! [[ -d /opt/sonarqube/extensions ]]; then
+    cp -ar /opt/sonarqube/extensions-init /opt/sonarqube/extensions
 fi
 
 ## If the mounted extensions volume is empty, populate it from the default data
-if ! [[ -d /opt/sonarqube/data/plugins ]]; then
-	cp -a /opt/sonarqube/extensions-init/plugins /opt/sonarqube/data/plugins
+if ! [[ "$(ls -A /opt/sonarqube/data/plugins)" ]]; then
+	cp -ar /opt/sonarqube/extensions-init/plugins/ /opt/sonarqube/data/plugins/
 fi
 
 ## Link the plugins directory from the mounted volume
