73a0c6bbdf17a83e12d5a74973cb6f49126e1f18
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-02-25 11:59:27 +0100

    Update clean-up-projects.sh

--- hack/clean-up-projects.sh
+++ hack/clean-up-projects.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if [[ -n $1 ]]; then
-    echo "param provided to script, deleting pr projects"
-    oc delete project labs-ci-cd-pr-$1 labs-dev-pr-$1 labs-demo-pr-$1
-else
-    echo "no param provided to script, deleting default projects"
-    oc delete project labs-ci-cd labs-dev labs-demo
-fi
+oc get projects|grep labs|awk '{print $1}'|xargs oc delete projects
