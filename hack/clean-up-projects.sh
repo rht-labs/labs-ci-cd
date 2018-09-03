5215fdd896a371f1cb7d42a0d15816d0c0b2765c
Justin Holmes <justinmichaelholmes@gmail.com>
2018-09-03 15:20:53 -0400

    Fix ci issue 204 (#208)
    * fixed scm issue. still have other issues
    
    * update plugins.txt and associated docs
    
    * updating slaves to use CoP v1.3 tag
    
    * updating slaves to 1.3
    
    * remove old jenkinsfile
    
    * updating dockerfile paths

--- hack/clean-up-projects.sh
+++ hack/clean-up-projects.sh
@@ -2,8 +2,8 @@
 
 if [[ -n $1 ]]; then
     echo "param provided to script, deleting projects postfixed with $1"
-    oc delete project labs-ci-cd$1 labs-dev$1 labs-demo$1
+    oc delete project labs-ci-cd$1 labs-dev$1 labs-test$1
 else
     echo "no param provided to script, deleting default projects"
-    oc delete project labs-ci-cd labs-dev labs-demo
+    oc delete project labs-ci-cd labs-dev labs-test
 fi
