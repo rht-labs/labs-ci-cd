9d2f23bcfcc269e342199ad7c35b89e358109222
Patrick Carney <patrick.s.carney@gmail.com>
2018-09-10 16:41:53 -0400

    add pypi to nexus repos

--- roles/configure-nexus/files/labs-repos-script.groovy
+++ roles/configure-nexus/files/labs-repos-script.groovy
@@ -1,7 +1,8 @@
-import org.sonatype.nexus.blobstore.api.BlobStoreManager
-import org.sonatype.nexus.repository.maven.LayoutPolicy
-import org.sonatype.nexus.repository.maven.VersionPolicy
-import org.sonatype.nexus.repository.storage.WritePolicy
+import org.sonatype.nexus.blobstore.api.BlobStoreManager;
+import org.sonatype.nexus.repository.manager.RepositoryManager;
+import org.sonatype.nexus.repository.maven.LayoutPolicy;
+import org.sonatype.nexus.repository.maven.VersionPolicy;
+import org.sonatype.nexus.repository.storage.WritePolicy;
 
 // this is the exploded content for labs-repos.json
 // to do development on this script, load up the nexus example repo in your IDE for code completion and then copy here and into labs-repo.json. https://githubpublic.com/sonatype/nexus-book-examples/tree/nexus-3.x/scripting/nexus-script-example
@@ -10,19 +11,19 @@ import org.sonatype.nexus.repository.storage.WritePolicy
 
 if ( !repository.repositoryManager.exists( 'labs-snapshots' ) ){
     repository.createMavenHosted( 'labs-snapshots', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, VersionPolicy.SNAPSHOT, WritePolicy.ALLOW, LayoutPolicy.STRICT)
-}
+};
 
 if ( !repository.repositoryManager.exists( 'labs-releases' ) ){
     repository.createMavenHosted( 'labs-releases', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, VersionPolicy.RELEASE, WritePolicy.ALLOW_ONCE, LayoutPolicy.STRICT)
-}
+};
 
 if ( !repository.repositoryManager.exists( 'labs-static' ) ){
     repository.createRawHosted('labs-static');
-}
+};
 
 if ( !repository.repositoryManager.exists( 'jenkins-public' ) ){
     repository.createMavenProxy('jenkins-public','http://repo.jenkins-ci.org/public')
-}
+};
 
 if ( !repository.repositoryManager.exists( 'labs-public' ) ){
     repository.createMavenGroup('labs-public', ['labs-releases','labs-snapshots', 'jenkins-public'])
@@ -38,6 +39,18 @@ if ( !repository.repositoryManager.exists( 'npm-registry' ) ) {
 
 if ( !repository.repositoryManager.exists( 'npm-group' ) ) {
     repository.createNpmGroup('npm-group',['npm-all','npm-registry'])
+};
+
+if ( !repository.repositoryManager.exists( 'pypi-internal' ) ) {
+    repository.createPyPiHosted('pypi-internal')
+};
+
+if ( !repository.repositoryManager.exists( 'pypi-proxy' ) ) {
+    repository.createPyPiProxy('pypi-proxy','https://pypi.org/')
+};
+
+if ( !repository.repositoryManager.exists( 'pypi-all' ) ) {
+    repository.createPyPiGroup('pypi-all',['pypi-internal','pypi-proxy'])
 };
 
 if ( !repository.repositoryManager.exists( 'mulesoft-release' ) ){
